b4dc9efc4646d49620d6f141c7be93827e9fd6ab
asharnadeem <ashar.nadeem@shipveho.com>
2022-04-24 10:14:40 -0400

    remove extranuous files

--- home.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    console.log(tracking)
-    var resume = tracking[tracking.length - 1]
-    console.log(resume)
-    $("#resume").click(function(){
-        window.location.assign(resume)
-    })
-});
