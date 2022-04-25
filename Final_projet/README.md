6bcebffb83a18b833ea16dd6ac2ef7593126228c
Yuan Lu <97483726+YuanLu-213@users.noreply.github.com>
2022-04-25 10:56:08 -0700

    Delete Final_projet directory

--- Final_projet/README.md
+++
@@ -1,3 +0,0 @@
-# COMS4170-Final-Project
-
-This repo contains all the code for COMS4170 UI design final project.
--- Final_projet/static/home.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-    var resume = tracking[tracking.length - 1]
-    $("#resume").click(function(){
-        window.location.assign(resume)
-    })
-});
