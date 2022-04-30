f753cf9168690e4819d210613c5eefd87cd82c99
asharnadeem <ashar@shipveho.com>
2022-04-30 16:10:19 -0400

    prettier

--- static/js/offensive.js
+++ static/js/offensive.js
@@ -1,21 +1,21 @@
-$(document).ready(function(){
-    let location = window.location.href;
-    console.log(location);
+$(document).ready(function () {
+  let location = window.location.href;
+  console.log(location);
 
-    $.ajax({
-        type: "POST",
-        url: "/track_learning",
-        dataType: "json",
-        contentType: "application/json; charset=utf-8",
-        data: JSON.stringify(location),
-        success: function(res){
-            console.log('succeed')
-        },
-        error: function(request, status, error){
-            console.log("Error");
-            console.log(request)
-            console.log(status)
-            console.log(error)
-        }
-    })
-})
+  $.ajax({
+    type: "POST",
+    url: "/track_learning",
+    dataType: "json",
+    contentType: "application/json; charset=utf-8",
+    data: JSON.stringify(location),
+    success: function (res) {
+      console.log("succeed");
+    },
+    error: function (request, status, error) {
+      console.log("Error");
+      console.log(request);
+      console.log(status);
+      console.log(error);
+    },
+  });
+});
--- static/js/quiz.js
+++ static/js/quiz.js
@@ -1,69 +1,67 @@
-$(document).ready(function(){
-    // $('h4').hide();
+$(document).ready(function () {
+  // $('h4').hide();
 
-    // $('#hint').click(function(){
-    //     $("h4").slideToggle();
-    // })
-    $("#dialog").dialog({
-        autoOpen: false,
-        modal: true
-    });
+  // $('#hint').click(function(){
+  //     $("h4").slideToggle();
+  // })
+  $("#dialog").dialog({
+    autoOpen: false,
+    modal: true,
+  });
+
+  $("#dialog-check").dialog({
+    autoOpen: false,
+    modal: true,
+  });
+
+  $("form").submit(function (event) {
+    if ($("input[name=radAnswer]:checked").length > 0) {
+      return;
+    }
 
     $("#dialog-check").dialog({
-        autoOpen: false,
-        modal: true
+      buttons: {
+        Confirm: function () {
+          $(this).dialog("close");
+        },
+      },
     });
 
-    $( "form" ).submit(function( event ) {
-        if ($('input[name=radAnswer]:checked').length > 0) {
-            return;
-        }
-    
-        $("#dialog-check").dialog({
-            buttons : {
-                "Confirm" : function() {
-                    $(this).dialog("close");
-                }
-            }
-        });
+    $("#dialog-check").dialog("open");
+    event.preventDefault();
+  });
 
-        $("#dialog-check").dialog("open");
-        event.preventDefault();
-    });
-
-    $('.again').click(function(e){
-        var targetUrl = '/quiz_start';
-        e.preventDefault();
-        $("#dialog").dialog({
-            buttons : {
-                "Confirm" : function() {
-                        window.location.href = targetUrl;
-                    },
-                "Cancel" : function() {
-                    $(this).dialog("close");
-                }
-            }
-        });
-
-        $("#dialog").dialog("open");  
-    })
-
-
-});
-
-$('#confirm').click(function(e){
-    var targetUrl = $(this).attr("href");
+  $(".again").click(function (e) {
+    var targetUrl = "/quiz_start";
     e.preventDefault();
     $("#dialog").dialog({
-        buttons : {
-            "Confirm" : function() {
-                    window.location.href = targetUrl;
-                },
-            "Cancel" : function() {
-                $(this).dialog("close");
-            }
-        }
+      buttons: {
+        Confirm: function () {
+          window.location.href = targetUrl;
+        },
+        Cancel: function () {
+          $(this).dialog("close");
+        },
+      },
     });
 
-    $("#dialog").dialog("open");  
+    $("#dialog").dialog("open");
+  });
+});
+
+$("#confirm").click(function (e) {
+  var targetUrl = $(this).attr("href");
+  e.preventDefault();
+  $("#dialog").dialog({
+    buttons: {
+      Confirm: function () {
+        window.location.href = targetUrl;
+      },
+      Cancel: function () {
+        $(this).dialog("close");
+      },
+    },
+  });
+
+  $("#dialog").dialog("open");
 });
