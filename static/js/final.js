f21b377105587407cd05d4869ff907665e010b52
ZiqiGuo0423 <94406343+ZiqiGuo0423@users.noreply.github.com>
2022-05-04 11:47:38 -0400

    Update final.js

--- static/js/final.js
+++ static/js/final.js
@@ -3,4 +3,21 @@ $(document).ready(function () {
     /* redirect to the home page */
     window.location.assign("/");
   });
+  if(userChoice.length > 0){
+    $.each(userChoice,function(index,value){
+      console.log(value);
+      console.log(index);
+      console.log(correctChoice[index]);
+      // let correctchoice = correctChoice[index];
+      let newindex = parseInt(index)+1;
+      let newindexs = newindex.toString();
+      console.log(newindexs);
+      if (value == correctChoice[index]){
+        $('#q' + newindexs).css({"background-color":"rgba(0, 239, 0, 0.49)","border-color":"rgba(0, 239, 0, 0.49)","color":"white"});
+      }
+      else{
+        $('#q' + newindexs).css({"background-color":"rgb(244 67 54 / 48%)","border-color":"rgb(244 67 54 / 48%)","color":"white"});
+      }
+    })
+  }
 });
